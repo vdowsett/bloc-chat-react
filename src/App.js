7ee197b7d33b1268a8f5ec04648b628b789d5710
vdowsett <vdowsett@gmail.com>
2018-10-19 11:52:29 -0500

    debugging firebase error

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC5zvN0EXhIUisEdms6SYLDzvUAogX9h-0",
+    authDomain: "bloc-chat-react-de285.firebaseapp.com",
+    databaseURL: "https://bloc-chat-react-de285.firebaseio.com",
+    projectId: "bloc-chat-react-de285",
+    storageBucket: "bloc-chat-react-de285.appspot.com",
+    messagingSenderId: "196906533860"
+  };
+  firebase.initializeApp(config);
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
         </header>
+
+        <body>
+          <section id="sidebar">
+            <h1>Chat App</h1>
+            <ul>
+              <RoomList />
+            </ul>
+          </section>
+          <section id="mainBody"></section>
+
+        </body>
       </div>
     );
   }
